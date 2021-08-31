8e2d46e5199854f74a6a347ceaf4ce9a6bd6c66b
Robertas <Robertas.Ankudovicius@ibm.com>
2021-08-31 22:26:47 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from 'styled-components'
 
-function App() {
+
+const InputsWrapper = styled('textarea')`
+width:300px;
+height:300px;
+`;
+
+ const HtmlCodeInptut = styled('textarea')`
+  width:300px;
+  height:300px;
+  `;
+
+const CssCodeInput = styled('textarea')`
+width:300px;
+height:300px;
+`;
+
+
+const App: React.FC =()=> {  
+  const [htmlCode, setHtmlCode] = useState<string>('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputsWrapper>
+        <HtmlCodeInptut onChange={(e)=>setHtmlCode(e.target.value)}/>
+        <CssCodeInput onChange={(e)=>setHtmlCode(e.target.value)}/>
+      </InputsWrapper>
+        {htmlCode}
     </div>
   );
 }
